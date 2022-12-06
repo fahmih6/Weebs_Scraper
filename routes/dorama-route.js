d23ecdad29c74c8a2ea3ce3c6f40c6d4e54710a1
Muhammad Fahmi Hidayat <36060021+fahmih6@users.noreply.github.com>
2022-12-06 12:33:33 +0700

    Dorama : Added Dorama API from doramaindo
    Dorama :
    
    [-] Added Dorama API from doramaindo.

--- routes/dorama-route.js
+++ routes/dorama-route.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 
-const animeService = require("../services/dorama-service.js");
+const doramaService = require("../services/dorama-service.js");
 
-router.get("/", (req, res) => animeService.getDorama(req, res));
+router.get("/", (req, res) => doramaService.getDorama(req, res));
+router.get("/:param", (req, res) => doramaService.getDoramaDetail(req, res));
+router.post("/get_video_link", (req, res) =>
+  doramaService.getVideoLink(req, res)
+);
 
 module.exports = router;
--- services/dorama-service.js
+++ services/dorama-service.js
@@ -1,8 +1,20 @@
+// @ts-nocheck
 const Crawler = require("crawler");
 const puppeteer = require("puppeteer");
 const arrayHelper = require("../helper/array-helper.js");
 
+const puppeteerOptions = {
+  args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-gpu"],
+  ignoreDefaultArgs: ["--disable-extensions"],
+};
+
+const pageOptions = {
+  waitUntil: "domcontentloaded",
+  timeout: 0,
+};
+
 module.exports.getDorama = async (req, res) => {
+  const url = req.protocol + "://" + req.get("host") + req.baseUrl;
   var c = new Crawler({
     // This will be called for each crawled page
     callback: (error, result, done) => {
@@ -21,16 +33,25 @@ module.exports.getDorama = async (req, res) => {
           let map = {};
 
           const title = $(el).find("h2").find("a").attr("title");
-          const link = $(el).find("h2").find("a").attr("href");
+          const link = $(el)
+            .find("h2")
+            .find("a")
+            .attr("href")
+            .replace(`${process.env.DORAMAINDO_LINK}`, `${url}`)
+            .replaceAll(`-`, `~`);
+
           const thumbnail = $(el)
             .find(".thumbnail")
             .find("a")
             .find("img")
-            .attr("src");
+            .attr("srcset");
+
+          const thumbnailList = thumbnail.split(" ");
+          const newThumbnail = thumbnailList[thumbnailList.length - 2];
 
           map["title"] = title;
           map["link"] = link;
-          map["thumbnail"] = thumbnail;
+          map["thumbnail"] = newThumbnail;
 
           list.push(map);
         });
@@ -44,50 +65,129 @@ module.exports.getDorama = async (req, res) => {
     },
   });
 
-  c.queue("https://dorama.doramaindo.ai/");
+  c.queue(`${process.env.DORAMAINDO_LINK}`);
 };
 
 module.exports.getDoramaDetail = async (req, res) => {
   const { param } = req.params;
+  let tempParam = param.replaceAll(/~/gi, "-");
   const url = req.protocol + "://" + req.get("host") + req.baseUrl;
 
-  var c = new Crawler({
-    // This will be called for each crawled page
-    callback: (error, result, done) => {
-      if (error) {
-        console.log(error);
+  /// Run Browser
+  const browser = await puppeteer.launch(puppeteerOptions);
 
-        res.json({
-          error: error.message,
-        });
-      } else {
-        var $ = result.$;
+  /// Page
+  const browserPage = await browser.newPage();
+  await browserPage.goto(
+    `${process.env.DORAMAINDO_LINK}/${tempParam}`,
+    pageOptions
+  );
 
-        var list = [];
+  /// Episode links
+  let episodeLinks = {};
 
-        $(".ep")
-          .find(".linkstream")
-          .each((i, el) => {
-            let map = {};
+  /// Episode container
+  const episodeContainer = await browserPage.$(".my-4");
+  const episodeElements = await episodeContainer.$$("ul");
 
-            const text = $(el).find("h4").text();
-            const link = $(el).find("a").attr("link");
+  /// Loop through all uls
+  for (let index = 0; index < episodeElements.length; index++) {
+    const element = episodeElements[index];
+    const text = `Episode ${index + 1}`;
 
-            map[text] = link;
+    /// Episode map
+    episodeLinks[text] = [];
 
-            list.push(map);
-          });
+    /// Reso links
+    const resoLinks = await element.$$("li");
+    for (const key in resoLinks) {
+      if (Object.hasOwnProperty.call(resoLinks, key)) {
+        const resoElement = resoLinks[key];
+        /// Resolution Text
+        const resoTextElement = await resoElement.$("strong");
+        const resoText = await resoTextElement.evaluate((el) => el.textContent);
 
-        res.json({
-          episodes: list,
-        });
+        /// Resolution Link
+        const resoLinkElements = await resoElement.$$("a");
+        for (const key in resoLinkElements) {
+          if (Object.hasOwnProperty.call(resoLinkElements, key)) {
+            const resoLinkElement = resoLinkElements[key];
+            const resoLink = await resoLinkElement.evaluate((el) => {
+              /// Return only zippyshare link
+              if (
+                el.getAttribute("href") &&
+                el.textContent.toLocaleLowerCase().includes("zippyshare")
+              ) {
+                return el.getAttribute("href");
+              }
+            });
+
+            /// Make sure resolution link is found
+            if (resoLink) {
+              let resoMap = {};
+              resoMap[resoText] = resoLink;
+              episodeLinks[text].push(resoMap);
+              break;
+            }
+          }
+        }
       }
+    }
+  }
 
-      done();
-    },
+  /// Judul
+  const judulElement = await browserPage.$(".jdl");
+  const judulText = await judulElement.evaluate((el) => el.textContent);
+
+  /// Sinopsis
+  const sinopsisElement = await browserPage.$(
+    "body > div.container > div:nth-child(6) > div.eight.columns > div.content-wrapper > article > div.text > div.text"
+  );
+  let sinopsisText = "";
+  const sinopsisTextElements = await sinopsisElement.$$("p");
+  for (const key in sinopsisTextElements) {
+    if (Object.hasOwnProperty.call(sinopsisTextElements, key)) {
+      const element = sinopsisTextElements[key];
+      const text = await element.evaluate((el) => el.textContent);
+      sinopsisText += ` ${text}`;
+    }
+  }
+
+  res.json({
+    title: judulText,
+    sinopsis: sinopsisText.trim(),
+    episode_links: episodeLinks,
   });
 
-  c.queue(
-    "https://dorama.doramaindo.ai/ase-to-sekken-2022-subtitle-indonesia.html"
-  );
+  await browserPage.close();
+};
+
+/// Get video link from zippyshare
+module.exports.getVideoLink = async (req, res) => {
+  const body = req.body;
+
+  if (body.hasOwnProperty("url")) {
+    const url = body["url"];
+    /// Run Browser
+    const browser = await puppeteer.launch(puppeteerOptions);
+
+    /// Page
+    const browserPage = await browser.newPage();
+    await browserPage.goto(url, pageOptions);
+    const lastUrl = await browserPage.$eval("#dlbutton", (el) =>
+      el.getAttribute("href")
+    );
+    const { hostname } = new URL(url);
+    const directLink = hostname + lastUrl;
+
+    await browserPage.close();
+
+    res.json({
+      link: directLink,
+    });
+  } else {
+    res.json({
+      error: "No URL Specified",
+    });
+  }
 };
