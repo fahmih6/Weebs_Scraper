77e35107d04ab8115d824e73b26567816b76c32b
Muhammad Fahmi Hidayat <36060021+fahmih6@users.noreply.github.com>
2026-02-20 16:43:21 +0700

    Komiku : Fixed Komiku Failed to Load Manga Detail. (#50)
    Komiku :
    
    - Fixed Komiku Failed to Load Manga Detail.

--- helper/komiku_helpers/komiku_helpers.js
+++ helper/komiku_helpers/komiku_helpers.js
@@ -8,42 +8,41 @@ class KomikuHelpers {
   static getMangaDetail = async (param, url) => {
     try {
       /// Get the data
-      const { data } = await axios.get(`https://komiku.id/manga/${param}`, {
+      const { data } = await axios.get(`https://komiku.org/manga/${param}/`, {
         proxy: false,
+        headers: { referer: "https://komiku.org/" },
       });
 
       // Load HTML we fetched in the previous line
       const $ = cheerio.load(data);
 
-      const mangaTitle = $("#Judul h1").text().trim();
+      const mangaTitle = $("h1").text().trim();
       const mangaThumbnail = $(".ims img").attr("src");
       const mangaGenre = [];
-      const mangaSynopsis = $(".desc").text().trim();
+      const mangaSynopsis = $(".sinopsis").text().trim();
       const mangaChapters = [];
       const mangaSimilar = [];
 
       // @ts-ignore
-      $(".genre li a").each((i, el) => {
+      $(".genre a").each((i, el) => {
         mangaGenre.push($(el).text().trim());
       });
 
-      $("#Daftar_Chapter tbody tr").each((i, el) => {
-        if (i > 0) {
-          const chapterNumber = $(el).find(".judulseries").text().trim();
-          // @ts-ignore
-          const chapterSlug = $(el)
-            .find(".judulseries")
-            .find("a")
-            .attr("href")
-            .split("/")[2];
+      $("#Daftar_Chapter tr").each((i, el) => {
+        const chapterNumber = $(el).find(".judulseries").text().trim();
+        if (chapterNumber && !chapterNumber.includes("Nomor Chapter")) {
+          const chapterLink = $(el).find("a").attr("href") ?? "";
+          const chapterSlug = chapterLink.split("/").filter(Boolean).pop();
           const chapterRelease = $(el).find(".tanggalseries").text().trim();
 
-          mangaChapters.push({
-            chapter: chapterNumber,
-            param: chapterSlug,
-            release: chapterRelease,
-            detail_url: `${url}/chapter/${chapterSlug}`,
-          });
+          if (chapterSlug) {
+            mangaChapters.push({
+              chapter: chapterNumber,
+              param: chapterSlug,
+              release: chapterRelease,
+              detail_url: `${url}/chapter/${chapterSlug}`,
+            });
+          }
         }
       });
 
@@ -58,10 +57,9 @@ class KomikuHelpers {
         // @ts-ignore
         .each((i, el) => {
           /// Spoiler param
-          const link = $(el).find("a").attr("href");
-          // @ts-ignore
-          const linkArray = link.split("/");
-          const spoilerParam = linkArray[linkArray.length - 2];
+          const link = $(el).find("a").attr("href") ?? "";
+          const linkArray = link.split("/").filter(Boolean);
+          const spoilerParam = linkArray.pop();
 
           /// Spoiler title
           const spoilerTitle = $(el).find(".h4").text().trim();
@@ -71,7 +69,7 @@ class KomikuHelpers {
           const spoilerThumbnail = $(el)
             .find("img")
             .attr("data-src")
-            .split("?")[0];
+            ?.split("?")[0];
 
           /// Synopsis
           const spoilerSynopsis = $(el).find("p").text().trim();
@@ -89,7 +87,7 @@ class KomikuHelpers {
       return {
         title: trimmedTitle,
         // @ts-ignore
-        thumbnail: mangaThumbnail.split("?")[0],
+        thumbnail: mangaThumbnail?.split("?")[0],
         genre: mangaGenre,
         synopsis: mangaSynopsis,
         chapters: mangaChapters,
@@ -100,7 +98,7 @@ class KomikuHelpers {
       return {
         data: {},
         error: {
-          error: err ?? "Unknown Error",
+          error: err.message ?? "Unknown Error",
         },
       };
     }
